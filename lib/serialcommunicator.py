761d869736d0f05bfd025d3645b10f0ce89c3c99
Wesam Nabeel <wesamezio@gmail.com>
2023-05-09 04:07:40 +0300

    remove print, remove unnecessary function calls

--- lib/serialcommunicator.py
+++ lib/serialcommunicator.py
@@ -6,8 +6,6 @@ class SerialCommunicator:
         self.__serial_port = serial_port
         self.__baudrate = baudrate
         self.__serial = serial.Serial(port=serial_port, baudrate=baudrate)
-        self.__serial.close()
-        self.__serial.open()
 
     def write_to_serial(self, data):
         self.__serial.write(str(data).encode(encoding="ascii"))
@@ -23,3 +21,6 @@ class SerialCommunicator:
 
     def close(self):
         self.__serial.close()
+
+    def open(self):
+        self.__serial.open()
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from utils import constants
 from lib.read_mnist import read_mnist_image
 from lib.serialcommunicator import SerialCommunicator
 
-mnist_image = read_mnist_image(constants.TEST_DATASET_PATH, row_index=352)
+sample = int(input("choose row:"))
+mnist_image = read_mnist_image(constants.TEST_DATASET_PATH, row_index=sample)
 
 try:
     serial_communicator = SerialCommunicator(constants.COM_PORT, constants.BAUDRATE)
@@ -18,7 +19,6 @@ try:
         count = count + 1
 
         arduino_output = serial_communicator.read_from_serial()
-        print(arduino_output)
 
     if count != 784:
         raise Exception("some data went missing")
