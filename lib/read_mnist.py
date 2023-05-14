4703936ec6e8b0fd25d050bba7c9378e161df185
Wesam Nabeel <wesamezio@gmail.com>
2023-05-14 16:33:24 +0300

    show image after selecting

--- lib/read_mnist.py
+++ lib/read_mnist.py
@@ -10,9 +10,10 @@ def read_mnist_image(file_path, row_index):
     sample_mnist_image = sample_mnist_row[constants.PIXELS_START_COLUMN:]
     sample_mnist_image_2d = sample_mnist_image.reshape(28, 28)
 
-    plt.figure()
+    plt.figure(1)
     plt.imshow(sample_mnist_image_2d)
     plt.savefig(constants.TEMP_SAVE_PATH)
+    plt.show()
 
     sample_mnist_label = sample_mnist_row[constants.LABEL_COLUMN]
     print("the label of mnist image is %d" % sample_mnist_label)
